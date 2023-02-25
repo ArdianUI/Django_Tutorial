02d7d3303c1df6f3b80ae558aa628bf772348627
Ardian <ardian11@ui.ac.id>
2023-02-25 11:51:08 +0700

    Update repo

--- money_tracker/views.py
+++ money_tracker/views.py
@@ -1,16 +1,55 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from money_tracker.models import TransactionRecord
+from django.http import HttpResponseRedirect
+from money_tracker.forms import TransactionRecordForm
+from django.urls import reverse
+from django.core import serializers
+
 
 
 # Create your views here.
 def show_tracker(request):
     transaction_data = TransactionRecord.objects.all()
+
     context = {
     'list_of_transactions': transaction_data,
     'name': 'Ardian'
     }
+
     return render(request, "tracker.html", context)
 
 def say_hello(request):
+
     return HttpResponse('Hello World')
+
+def create_transaction(request):
+    form = TransactionRecordForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('money_tracker:show_tracker'))
+
+    context = {'form': form}
+
+    return render(request, "create_transaction.html", context)
+
+def show_xml(request):
+    data = TransactionRecord.objects.all()
+
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = TransactionRecord.objects.all()
+
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = TransactionRecord.objects.filter(pk=id)
+
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = TransactionRecord.objects.filter(pk=id)
+
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
