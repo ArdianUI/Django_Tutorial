ef499fd51acb2f625469bec5276f6b49fa7d4877
Ardian <ardian11@ui.ac.id>
2023-03-08 13:30:16 +0700

    Membuat Fungsi Baru untuk Menghapus Data Transaksi

--- money_tracker/urls.py
+++ money_tracker/urls.py
@@ -8,6 +8,8 @@ from money_tracker.views import register
 from money_tracker.views import login_user 
 from money_tracker.views import logout_user
 from money_tracker.views import modify_transaction
+from money_tracker.views import delete_transaction
+
 
 
 
@@ -24,4 +26,5 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('modify/<int:id>', modify_transaction, name='modify_transaction'),
+    path('delete/<int:id>', delete_transaction, name='delete_transaction'), 
 ]
